ab2ad8613a3e2ef1ec2078b18235f3023e8c8a85
Marta Mauri <46056167+martamau@users.noreply.github.com>
2021-07-21 22:10:38 +0200

    Thermal target data (#53)
    * A new file that generates target distribution data for coherent thermal states. And a test script.
    
    * Tidied up the files so that they have the same structure as targte.py
    
    * Update target_thermal_states.py
    
    * Reversed the string to match qiskit layout
    
    * target_asset_data for portfolio optimization on the qcbm
    
    * Added way to save to save thermal target states to json file
    
    * Updated Asset Target Data
    
    * Changes to asset target data
    
    * Tracking number of circuits run
    
    * Changes for QCBM and Metalearning Workflow
    
    * Changes for workflow
    
    * keeping circuit evals
    
    * Updated thermal target data for improved target distribution as well as sampling distribution
    
    * changes because of target thermal data file changes
    
    * changes
    
    * changes
    
    * Added save/load functions for QCBMAnsatz problems as well as from_dict, to_dict functions.
    
    * Fixed syntax errors
    
    * Small syntax changes
    
    * adding save_ansatz_set + tests
    
    * fixing errors
    
    * update thermal target data (#37)
    
    * update thermal target data
    
    * fixing errors
    
    * removing asset data from this branch
    
    * removing temp files
    
    * minor tweaks
    
    * adding integration tests for thermal data + minor tweaks
    
    * trying to reduce runtime
    
    * Updated qcbm ansatz to new circuits.
    
    * fixup! Updated qcbm ansatz to new circuits.
    
    * Fix and improve test for QCBM ansatz.
    
    * Refactor ansatz tests.
    
    * added changes for feature mapping
    
    * removing old test
    
    * changes for unit cases
    
    * import fix
    
    * changes for unit cases
    
    * changes for unit cases
    
    * changes for unit cases
    
    * changes for unit cases
    
    * Fix unknown symbols error in ansatz serialization (#48)
    
    * took out reveresed bitstrings
    
    * changes made from pull request comments
    
    * few changes
    
    * moving unti test to tests folder
    
    * fixing compatibility with new backend interface
    
    * changes made from pull request comments
    
    * fixing tests
    
    * changes made from pull request comments
    
    * adding tests
    
    * separating cardinality distribution and sampled distribution
    
    * Added comment.
    
    Co-authored-by: kaitlinmgili <kaitlinmgili@gmail.com>
    Co-authored-by: kaitlinmgili <40485018+kaitlinmgili@users.noreply.github.com>
    Co-authored-by: simonwa7 <simonwa7@gmail.com>
    Co-authored-by: mstechly <michal.stechly@gmail.com>
    Co-authored-by: Alexander Juda <6004040+alexjuda@users.noreply.github.com>

---
+++ src/orquestra/quantum/distributions/target_thermal_states.py
@@ -0,0 +1,176 @@
+import bisect
+import typing
+import numpy as np
+from zquantum.core.bitstring_distribution import BitstringDistribution
+from zquantum.core.utils import (
+    dec2bin,
+    bin2dec,
+    convert_tuples_to_bitstrings,
+    sample_from_probability_distribution,
+)
+
+
+def convert_integer_to_ising_bitstring(number: int, length: int) -> typing.List[int]:
+    """Converts an integer into a +/-1s bitstring (also called Ising bitstring).
+    Args:
+        number: positive number to be converted into its corresponding Ising bitstring representation
+        length: length of the Ising bitstring (i.e. positive number of spins in the Ising system)
+    Returns:
+        Ising bitstring representation (1D array of +/-1)."""
+
+    if length < 0:
+        raise ValueError("Length cannot be negative.")
+    binary_bitstring = dec2bin(number, length)
+    ising_bitstring = [bit * 2 - 1 for bit in binary_bitstring]
+    return ising_bitstring
+
+
+def convert_ising_bitstring_to_integer(ising_bitstring: typing.List[int]) -> int:
+    """Converts a +/-1s bitstring (also called Ising bitstring) into an integer.
+    Args:
+        ising_bitstring: 1D array of +/-1.
+    Returns:
+        Integer number representation of the Ising bitstring."""
+
+    binary_bitstring = [int((bit + 1) / 2) for bit in ising_bitstring]
+    number = bin2dec(binary_bitstring)
+    return number
+
+
+def _get_random_ising_hamiltonian_parameters(
+    n_spins: int,
+) -> typing.Tuple[np.ndarray, np.ndarray]:
+    """Generates random h, J, and where h and J are arrays of random coefficients sampled from a normal distribution with zero mean and sqrt(n_spins) sd.
+    For reproducibilty, fix random generator seed in the higher level from which this function is called.
+    Useful in the following Ising Hamiltonian: 1D Nearest Neighbor Ising Model with open boundary conditions,
+    where h's are the external field values and J's the two-body couplings coefficients.
+    Args:
+        n_spins (int): positive number of spins in the Ising system.
+    Returns:
+       external_fields (array): n_spin coefficients sampled from a normal distribution with zero mean and sqrt(n_spins) sd
+       two_body_couplings (array): n_spin x n_spin symmetric array of coefficients sampled from a normal distribution with zero mean and sqrt(n_spins) sd
+    """
+    external_fields = np.zeros((n_spins))
+    two_body_couplings = np.zeros((n_spins, n_spins))
+    for i in range(n_spins):
+        external_fields[i] = np.random.normal(0, np.sqrt(n_spins))
+        for j in range(i, n_spins):
+            if i == j:
+                continue
+            two_body_couplings[i, j] = two_body_couplings[j, i] = np.random.normal(
+                0, np.sqrt(n_spins)
+            )
+    return external_fields, two_body_couplings
+
+
+def get_thermal_target_bitstring_distribution(
+    n_spins: int,
+    temperature: float,
+    hamiltonian_parameters: typing.Tuple[np.ndarray, np.ndarray],
+) -> BitstringDistribution:
+    """Generates thermal states target distribution, saved in a dict where keys are bitstrings and
+    values are corresponding probabilities according to the Boltzmann Distribution formula.
+
+    Args:
+        n_spins: positive number of spins in the Ising system
+        temperature: temperature factor in the boltzman distribution
+        hamiltonian_parameters: values of hamiltonian parameters, namely external fields and two body couplings.
+
+    Returns:
+       Thermal target distribution.
+       Number of positive spins in the spin state.
+    """
+    partition_function = 0
+    external_fields, two_body_couplings = hamiltonian_parameters
+    beta = 1.0 / temperature
+    distribution = {}
+
+    for spin in range(int(2 ** n_spins)):
+        ising_bitstring = convert_integer_to_ising_bitstring(spin, n_spins)
+        energy = 0
+        for i in range(n_spins):
+            energy -= ising_bitstring[i] * external_fields[i]
+            if i != n_spins - 1:
+                energy -= (
+                    ising_bitstring[i]
+                    * ising_bitstring[i + 1]
+                    * two_body_couplings[i, i + 1]
+                )
+        boltzmann_factor = np.exp(energy * beta)
+        partition_function += boltzmann_factor
+
+        binary_bitstring = convert_tuples_to_bitstrings([dec2bin(spin, n_spins)])[0]
+        distribution[binary_bitstring] = boltzmann_factor
+
+    normalized_distribution = {
+        key: value / partition_function for key, value in distribution.items()
+    }
+
+    return BitstringDistribution(normalized_distribution)
+
+
+def get_thermal_sampled_distribution(
+    n_samples: int,
+    n_spins: int,
+    temperature: float,
+    hamiltonian_parameters: typing.Tuple[np.ndarray, np.ndarray],
+) -> BitstringDistribution:
+    """Generates thermal states sample distribution
+    Args:
+        n_samples: the number of samples from the original distribution
+        n_spins: number of spins in the Ising system
+        temperature: temperature factor in the Boltzmann distribution
+    Returns:
+       histogram_samples: keys are binary string representations and values are corresponding probabilities.
+    """
+    distribution = get_thermal_target_bitstring_distribution(
+        n_spins, temperature, hamiltonian_parameters
+    ).distribution_dict
+    sample_distribution_dict = sample_from_probability_distribution(
+        distribution, n_samples
+    )
+    histogram_samples = np.zeros(2 ** n_spins)
+    for samples, counts in sample_distribution_dict.items():
+        integer_list: typing.List[int] = []
+        for elem in samples:
+            integer_list.append(int(elem))
+        idx = convert_ising_bitstring_to_integer(integer_list)
+        histogram_samples[idx] += counts / n_samples
+
+    for spin in range(int(2 ** n_spins)):
+        binary_bitstring = convert_tuples_to_bitstrings([dec2bin(spin, n_spins)])[0]
+        sample_distribution_dict[binary_bitstring] = histogram_samples[spin]
+
+    return BitstringDistribution(sample_distribution_dict)
+
+
+def get_cardinality_distribution(
+    n_samples: int, n_spins: int, sampled_distribution: BitstringDistribution
+) -> typing.List[int]:
+    """Generates a list with all the occurrences associated to different cardinalities in a sampled distribution.
+
+    Args:
+        n_samples: the number of samples used to build the sampled distribution (used for normalization purposes)
+        n_spins: positive number of spins in the Ising system
+        sampled_distribution: bitstring distribution built of samples drawn for a target distribution
+
+    Returns:
+        cardinality_list: a list with the cardinalities of all the sampled bitstrings.
+
+    """
+    histogram_samples = np.zeros(2 ** n_spins)
+    cardinality_list: typing.List[int] = []
+    for samples, counts in sampled_distribution.distribution_dict.items():
+        integer_list: typing.List[int] = []
+        for elem in samples:
+            integer_list.append(int(elem))
+        idx = convert_ising_bitstring_to_integer(integer_list)
+        histogram_samples[idx] += counts / n_samples
+        cardinality = 0
+        for elem in integer_list:
+            if elem == 1:
+                cardinality += 1
+        for _ in range(int(counts)):
+            cardinality_list.append(cardinality)
+
+    return cardinality_list
---
+++ tests/orquestra/quantum/distribution/target_thermal_states_test.py
@@ -0,0 +1,212 @@
+import unittest
+import numpy as np
+from zquantum.core.utils import dec2bin, convert_tuples_to_bitstrings
+from zquantum.core.bitstring_distribution import BitstringDistribution
+from zquantum.core.bitstring_distribution.distance_measures.mmd import compute_mmd
+from zquantum.qcbm.target_thermal_states import (
+    convert_integer_to_ising_bitstring,
+    convert_ising_bitstring_to_integer,
+    _get_random_ising_hamiltonian_parameters,
+    get_thermal_target_bitstring_distribution,
+    get_thermal_sampled_distribution,
+    get_cardinality_distribution,
+)
+
+
+SEED = 14943
+
+
+class TestThermalTarget(unittest.TestCase):
+    def test_convert_integer_to_ising_bitstring(self):
+        # Given
+        number = 4
+        length = 5
+        expected_ising_bitstring = [-1, -1, 1, -1, -1]
+
+        # When
+        ising_bitstring = convert_integer_to_ising_bitstring(number, length)
+
+        # Then
+        self.assertListEqual(ising_bitstring, expected_ising_bitstring)
+
+        # When
+        ising_bitstring_zero = convert_integer_to_ising_bitstring(0, length)
+
+        # Then
+        self.assertListEqual(ising_bitstring_zero, [-1, -1, -1, -1, -1])
+
+    def test_convert_ising_bitstring_to_integer(self):
+        # Given
+        ising_bitstring = [-1, -1, 1, -1, -1]
+        expected_number = 4
+        zero_bitstring = [-1, -1, -1, -1, -1]
+
+        # When
+        number = convert_ising_bitstring_to_integer(ising_bitstring)
+
+        # Then
+        self.assertEqual(number, expected_number)
+
+        # When
+        number = convert_ising_bitstring_to_integer(zero_bitstring)
+
+        # Then
+        self.assertEqual(number, 0)
+
+    def test_ising2int2ising(self):
+        # Given
+        expected_number = 14
+        length = 5
+        zero_number = 0
+        zero_length = 0
+        # When
+        number = convert_ising_bitstring_to_integer(
+            convert_integer_to_ising_bitstring(expected_number, length)
+        )
+
+        # Then
+        self.assertEqual(number, expected_number)
+
+        # When
+        number = convert_ising_bitstring_to_integer(
+            convert_integer_to_ising_bitstring(zero_number, length)
+        )
+
+        # Then
+        self.assertEqual(number, zero_number)
+
+        # When
+        number = convert_ising_bitstring_to_integer(
+            convert_integer_to_ising_bitstring(zero_number, zero_length)
+        )
+
+        # Then
+        self.assertEqual(number, zero_number)
+
+    def test_get_random_ising_hamiltonian_parameters(self):
+        # Given
+        np.random.seed(seed=SEED)
+        n_spins = 15
+
+        # When
+        hamiltonian_parameters = _get_random_ising_hamiltonian_parameters(n_spins)
+
+        # Then
+        self.assertEqual(
+            len(hamiltonian_parameters[0]),
+            n_spins,
+        )
+        self.assertEqual(
+            hamiltonian_parameters[1].shape,
+            (n_spins, n_spins),
+        )
+
+    def test_get_thermal_target_distribution_dict(self):
+        # Given
+        n_spins = 5
+        temperature = 1.0
+        expected_bitstrings = convert_tuples_to_bitstrings(
+            [dec2bin(number, n_spins) for number in range(int(2 ** n_spins))]
+        )
+        expected_keys = expected_bitstrings[len(expected_bitstrings) :: -1]
+        np.random.seed(SEED)
+        external_fields = np.random.rand(n_spins)
+        two_body_couplings = np.random.rand(n_spins, n_spins)
+        hamiltonian_parameters = [external_fields, two_body_couplings]
+
+        # When
+        target_distribution = get_thermal_target_bitstring_distribution(
+            n_spins, temperature, hamiltonian_parameters
+        )
+
+        # Then
+        self.assertListEqual(
+            sorted(list(target_distribution.distribution_dict.keys())),
+            sorted(expected_keys),
+        )
+
+        self.assertAlmostEqual(
+            sum(list(target_distribution.distribution_dict.values())),
+            1.0,
+        )
+
+    def test_thermal_sampled_distribution(self):
+        # Given
+        n_samples = 5000
+        n_spins = 2
+        temperature = 0.85
+        np.random.seed(SEED)
+        external_fields = np.random.rand(n_spins)
+        two_body_couplings = np.random.rand(n_spins, n_spins)
+        hamiltonian_parameters = [external_fields, two_body_couplings]
+        expected_bitstrings = convert_tuples_to_bitstrings(
+            [dec2bin(number, n_spins) for number in range(int(2 ** n_spins))]
+        )
+        expected_keys = expected_bitstrings[len(expected_bitstrings) :: -1]
+
+        # When
+        sample_distribution = get_thermal_sampled_distribution(
+            n_samples, n_spins, temperature, hamiltonian_parameters
+        )
+
+        # Then
+        self.assertListEqual(
+            sorted(list(sample_distribution.distribution_dict.keys())),
+            sorted(expected_keys),
+        )
+
+        self.assertAlmostEqual(
+            sum(list(sample_distribution.distribution_dict.values())), 1
+        )
+
+    def test_samples_from_distribution(self):
+        # Given
+        n_samples = 10000
+        n_spins = 4
+        temperature = 1.0
+        distance_measure_parameters = {"sigma": 1.0}
+        np.random.seed(SEED)
+        external_fields = np.random.rand(n_spins)
+        two_body_couplings = np.random.rand(n_spins, n_spins)
+        hamiltonian_parameters = [external_fields, two_body_couplings]
+        np.random.seed(SEED)
+        actual = get_thermal_target_bitstring_distribution(
+            n_spins,
+            temperature,
+            hamiltonian_parameters,
+        )
+        np.random.seed(SEED)
+        model = get_thermal_sampled_distribution(
+            n_samples, n_spins, temperature, hamiltonian_parameters
+        )
+
+        # When
+        mmd = compute_mmd(actual, model, distance_measure_parameters)
+
+        # Then
+        self.assertLess(mmd, 1e-4)
+
+    def test_cardinality_distribution(self):
+        # Given
+        n_samples = 10000
+        n_spins = 4
+        temperature = 1.0
+        np.random.seed(SEED)
+        external_fields = np.random.rand(n_spins)
+        two_body_couplings = np.random.rand(n_spins, n_spins)
+        hamiltonian_parameters = [external_fields, two_body_couplings]
+        sampled_distribution = get_thermal_sampled_distribution(
+            n_samples, n_spins, temperature, hamiltonian_parameters
+        )
+
+        # When
+        cardinality_distr = get_cardinality_distribution(
+            n_samples, n_spins, sampled_distribution
+        )
+
+        # Then
+        assert all([spin_cardinality >= 0 for spin_cardinality in cardinality_distr])
+
+
+if __name__ == "__main__":
+    unittest.main()
