8b29ff93373b17c35088aef653e5b93b23899800
Shiro-Raven <exclass9.24@gmail.com>
2021-10-22 12:23:21 +0200

    ZQS-813 renamed bitstring_distribution to ditsequence_distribution, renamed root folder to distribution, added extra tests with some refactoring, flattened distance measures

---
+++ src/orquestra/quantum/distribution/jensen_shannon_divergence.py
@@ -0,0 +1,41 @@
+from typing import TYPE_CHECKING, Dict
+
+from .clipped_negative_log_likelihood import compute_clipped_negative_log_likelihood
+
+if TYPE_CHECKING:
+    from zquantum.core.distribution import DitSequenceDistribution
+
+
+def compute_jensen_shannon_divergence(
+    target_distribution: "DitSequenceDistribution",
+    measured_distribution: "DitSequenceDistribution",
+    distance_measure_parameters: Dict,
+) -> float:
+    """Computes the symmetrized version of the clipped negative log likelihood between a
+     target bitstring distribution and a measured bitstring distribution.
+    See Equation (4) in https://advances.sciencemag.org/content/5/10/eaaw9918?rss=1
+
+    Args:
+        target_distribution: The target bitstring probability distribution.
+        measured_distribution: The measured bitstring probability distribution.
+
+        distance_measure_parameters:
+            epsilon (float): The small parameter needed to regularize log computation
+            when argument is zero. The default value is 1e-9.
+
+    Returns:
+        float: The value of the symmetrized version
+    """
+
+    value = (
+        compute_clipped_negative_log_likelihood(
+            target_distribution, measured_distribution, distance_measure_parameters
+        )
+        / 2
+        + compute_clipped_negative_log_likelihood(
+            measured_distribution, target_distribution, distance_measure_parameters
+        )
+        / 2
+    )
+
+    return value
