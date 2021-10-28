275ee7629b26eea05f607e070b98e8be25d66031
mstechly <michal.stechly@gmail.com>
2021-10-27 21:43:21 -0400

    Fixed flake8 issues.

--- tests/orquestra/quantum/distribution/BAS_dataset_test.py
+++ tests/orquestra/quantum/distribution/BAS_dataset_test.py
@@ -2,16 +2,19 @@ import unittest
 import numpy as np
 from zquantum.core.bitstring_distribution import BitstringDistribution
 
-from zquantum.qcbm.target import ( get_num_bars_and_stripes_patterns,  get_bars_and_stripes_target_distribution,  bars_and_stripes_zigzag)
+from zquantum.qcbm.target import (
+    get_num_bars_and_stripes_patterns,
+    get_bars_and_stripes_target_distribution,
+    bars_and_stripes_zigzag,
+)
 
 
 class TestTarget(unittest.TestCase):
-
     def setUp(self):
-        self.methods = [ "zigzag" ]
-        self.grids = [ [1, 1], [1, 2], [2, 1], [2, 2], [3, 2], [2, 3], [3, 3]]
-        self.number_of_patterns = [2, 4, 4, 6, 10, 10, 14 ]
-        self.fractions = [ 1., 0.75, 0.5, 0.25, 0.1 ]
+        self.methods = ["zigzag"]
+        self.grids = [[1, 1], [1, 2], [2, 1], [2, 2], [3, 2], [2, 3], [3, 3]]
+        self.number_of_patterns = [2, 4, 4, 6, 10, 10, 14]
+        self.fractions = [1.0, 0.75, 0.5, 0.25, 0.1]
 
     def test_get_num_bars_and_stripes_patterns(self):
         for i, grid in enumerate(self.grids):
@@ -26,12 +29,23 @@ class TestTarget(unittest.TestCase):
             for i, grid in enumerate(self.grids):
                 for fraction in self.fractions:
                     # When
-                    bars_and_stripes_distribution = get_bars_and_stripes_target_distribution(grid[0], grid[1],  fraction=fraction, method=method)
-                    
+                    bars_and_stripes_distribution = (
+                        get_bars_and_stripes_target_distribution(
+                            grid[0], grid[1], fraction=fraction, method=method
+                        )
+                    )
+
                     # Then
-                    self.assertIsInstance(bars_and_stripes_distribution, BitstringDistribution)
-                    expected_num_patters = max(int(self.number_of_patterns[i]*fraction), 1)
-                    self.assertEqual(len(bars_and_stripes_distribution.distribution_dict.keys()), expected_num_patters)
+                    self.assertIsInstance(
+                        bars_and_stripes_distribution, BitstringDistribution
+                    )
+                    expected_num_patters = max(
+                        int(self.number_of_patterns[i] * fraction), 1
+                    )
+                    self.assertEqual(
+                        len(bars_and_stripes_distribution.distribution_dict.keys()),
+                        expected_num_patters,
+                    )
 
     def test_get_bars_and_stripes_target_distribution_unsupported_method(self):
         # Given
@@ -40,20 +54,18 @@ class TestTarget(unittest.TestCase):
         ncols = 2
 
         # When/Then
-        self.assertRaises(RuntimeError, lambda: get_bars_and_stripes_target_distribution(nrows, ncols, fraction=1., method=method))
+        self.assertRaises(
+            RuntimeError,
+            lambda: get_bars_and_stripes_target_distribution(
+                nrows, ncols, fraction=1.0, method=method
+            ),
+        )
 
     def test_bars_and_stripes_zigzag_2_by_2(self):
         # Given
         ncols = 2
         nrows = 2
-        expected_patterns = [ 
-            "0000",
-            "0011",
-            "1100",
-            "1010",
-            "0101",
-            "1111"
-        ]
+        expected_patterns = ["0000", "0011", "1100", "1010", "0101", "1111"]
 
         # When
         pattern_list = bars_and_stripes_zigzag(nrows, ncols)
@@ -65,6 +77,3 @@ class TestTarget(unittest.TestCase):
             for qubit in pattern:
                 bitstring += str(qubit)
             self.assertIn(bitstring, expected_patterns)
-        
-
-
