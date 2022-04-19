7f4ac384b17ea49911bd2f3757f37fa331541b8a
mstechly <michal.stechly@gmail.com>
2022-04-19 16:15:39 -0400

    feat: added missing SymbolicSimulator tests.

--- tests/orquestra/quantum/symbolic_simulator_test.py
+++ tests/orquestra/quantum/symbolic_simulator_test.py
@@ -1,7 +1,19 @@
+import numpy as np
 import pytest
 import sympy
 
-from orquestra.quantum import circuits
+from orquestra.quantum.circuits import (
+    CNOT,
+    RX,
+    RY,
+    U3,
+    XX,
+    Circuit,
+    GateOperation,
+    MultiPhaseOperation,
+    Operation,
+)
+from orquestra.quantum.interfaces.backend import QuantumSimulator
 from orquestra.quantum.interfaces.backend_test import (
     QuantumSimulatorGatesTest,
     QuantumSimulatorTests,
@@ -19,10 +31,22 @@ def wf_simulator() -> SymbolicSimulator:
     return SymbolicSimulator()
 
 
+class SymbolicSimulatorWithNonSupportedOperations(SymbolicSimulator):
+    def is_natively_supported(self, operation: Operation) -> bool:
+        if isinstance(operation, GateOperation) and operation.gate.name == "RX":
+            return False
+        return super().is_natively_supported(operation)
+
+
+class SymbolicSimulatorWithDefaultSetOfSupportedOperations(SymbolicSimulator):
+    def is_natively_supported(self, operation: Operation) -> bool:
+        return QuantumSimulator.is_natively_supported(self, operation)
+
+
 class TestSymbolicSimulator(QuantumSimulatorTests):
     gates_list = [
-        circuits.XX(sympy.Symbol("theta"))(2, 1),
-        circuits.U3(
+        XX(sympy.Symbol("theta"))(2, 1),
+        U3(
             sympy.Symbol("alpha"),
             sympy.Symbol("beta"),
             sympy.Symbol("gamma"),
@@ -48,7 +72,7 @@ class TestSymbolicSimulator(QuantumSimulatorTests):
         self, wf_simulator, gate, binding
     ):
 
-        circuit = circuits.Circuit([gate])
+        circuit = Circuit([gate])
         with pytest.raises(ValueError):
             wf_simulator.run_circuit_and_measure(
                 circuit, n_samples=1000, symbol_map=binding
@@ -59,9 +83,35 @@ class TestSymbolicSimulator(QuantumSimulatorTests):
         list(zip(gates_list, correct_bindings)),
     )
     def test_passes_for_complete_bindings(self, wf_simulator, gate, binding):
-        circuit = circuits.Circuit([gate])
+        circuit = Circuit([gate])
         wf_simulator.run_circuit_and_measure(
             circuit, n_samples=1000, symbol_map=binding
+        )
+
+    @pytest.mark.parametrize(
+        "circuit",
+        [
+            Circuit([RY(0.5)(0), RX(1)(1), CNOT(0, 2), RX(np.pi)(2)]),
+            Circuit([RX(1)(1), CNOT(0, 2), RX(np.pi)(2), RY(0.5)(0)]),
+            Circuit([RX(1)(1), CNOT(0, 2), RX(np.pi)(2), RY(0.5)(0), RX(0.5)(0)]),
+        ],
+    )
+    def test_quantum_simulator_switches_between_native_and_nonnative_modes_of_execution(
+        self, circuit
+    ):
+        simulator = SymbolicSimulatorWithNonSupportedOperations()
+        reference_simulator = SymbolicSimulator()
+
+        np.testing.assert_array_equal(
+            simulator.get_wavefunction(circuit).amplitudes,
+            reference_simulator.get_wavefunction(circuit).amplitudes,
+        )
+
+    def test_by_default_only_gate_operations_are_supported(self):
+        simulator = SymbolicSimulatorWithDefaultSetOfSupportedOperations()
+        assert simulator.is_natively_supported(RX(np.pi / 2)(1))
+        assert not simulator.is_natively_supported(
+            MultiPhaseOperation((0.5, 0.2, 0.3, 0.1))
         )
 
 
