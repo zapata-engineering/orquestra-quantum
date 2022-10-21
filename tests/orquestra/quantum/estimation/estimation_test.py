fix: random error for averaging non eigen states

tests/orquestra/quantum/estimation/estimation_test.py
@@ -411,7 +411,7 @@ TEST_CASES_NONEIGENSTATES = [
             EstimationTask(
                 PauliTerm("Z0", coefficient=-2),
                 circuit=Circuit([RY(np.pi / 4)(0)]),
-                number_of_shots=100000,
+                number_of_shots=10000,
             ),
         ],
         [