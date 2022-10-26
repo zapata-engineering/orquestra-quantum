b682b0851df588f2ffd6acde7aa01c55a30562aa
Konrad Jałowiecki <dexter2206@gmail.com>
2022-10-26 16:37:08 +0200

    feat: export wavefunction simulator

--- src/orquestra/quantum/api/__init__.py
+++ src/orquestra/quantum/api/__init__.py
@@ -1,3 +1,4 @@
 from .backend import QuantumBackend
 from .circuit_runner import BaseCircuitRunner, CircuitRunner
+from .wavefunction_simulator import BaseWavefunctionSimulator, WavefunctionSimulator
 from .estimation import EstimationTask
