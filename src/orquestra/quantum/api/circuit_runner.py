9bdeabc1c89dcb321cde93641e4a1a233f4e4fe8
Konrad Jałowiecki <dexter2206@gmail.com>
2022-12-02 01:33:05 +0100

    Update docstrings for wavefunction simulator (#73)
    * fix: typo (double space)
    
    * docs: add docstring for circuit_runner module
    
    * docs: add docstring for WavefunctionSimulator and BaseWavefunctionSimulator

--- src/orquestra/quantum/api/circuit_runner.py
+++ src/orquestra/quantum/api/circuit_runner.py
@@ -1,6 +1,7 @@
 ################################################################################
 # © Copyright 2022 Zapata Computing Inc.
 ################################################################################
+"""Definition of CircuitRunner protocol and ABC for implementing it."""
 from abc import ABC, abstractmethod
 from typing import List, Optional, Protocol, Sequence, Union
 
@@ -180,7 +181,7 @@ class BaseCircuitRunner(ABC, CircuitRunner):
 
     @abstractmethod
     def _run_and_measure(self, circuit: Circuit, n_samples: int) -> Measurements:
-        """Underlying implementation of running and measuring  single circuit.
+        """Underlying implementation of running and measuring single circuit.
 
         Implementations of this method can assume that n_samples is positive.
         Furthermore, implementations of this method should not modify
